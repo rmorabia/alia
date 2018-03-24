85f98d045a2df27aa42b2de9236a6a83bf1892a4
Radhika Morabia <hi@rmorabia.com>
2018-03-24 00:24:35 -0700

    v2.0.0: Upgrade to JavaScript Promises + real README!

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # When is Alia Bhatt's next movie coming out?
 
-A silly dynamic app made with loads & loads of help from [TMDB](http://themoviedb.org).
+This app counts down to Alia Bhatt's next movie release.
 
-Responsive YouTube video code from: https://avexdesigns.com/responsive-youtube-embed/
+![](alia.gif)
+
+## Technologies Used:
+
+* JavaScript
+* ES6 Promises
+* AJAX
+* GET API
+* [TMDb API](http://developers.themoviedb.org)
+* HTML
+* CSS
+
+Both the CSS and the JavaScript are framework-free.
+
+## Features
+
+* Live countdown timer
+* Update on refresh
+* Pulls text, image, and video data from TMDb
+* Mobile-responsive
+* If a YouTube trailer exists, display it
+* Error catching if the request fails or if the request returns nothing
+
+## Special Thanks
+
+The [TMDb API](http://developers.themoviedb.org) is extremely open and well-documented. Without their liberal use policy, this wouldn't have been possible.
+
+In order to make the YouTube trailer responsive, I borrowed code from [this article](https://avexdesigns.com/responsive-youtube-embed/).
--- script.js
+++ script.js
@@ -1,138 +1,129 @@
-// Global variables because I don't understand how to handle this without making everything a nested hellscape
+// Initial promise initialization
 
-let date
-let release
-let aliaRequest
-let credits
-let releaseIndex
-let movieRequest
-let movieCredits
-let movieVideo
-
-// Initial http request
-
-;
-(() => {
-  aliaRequest = new XMLHttpRequest()
-  aliaRequest.open('GET', 'https://api.themoviedb.org/3/person/1108120/movie_credits?api_key=f8e4d97cbdd172b25e1dd31546263dcd&language=en-US')
-  aliaRequest.send()
-  aliaRequest.onreadystatechange = getReleaseDate
-})()
-
-// Date-related functions
-
-function getReleaseDate() {
-  if (aliaRequest.readyState === XMLHttpRequest.DONE) {
-    getDate()
-    credits = JSON.parse(aliaRequest.response)
-    let releaseDate = []
-    for (let i = 0; i < credits.cast.length; i++) {
-      releaseDate[i] = credits.cast[i].release_date
+function get (url) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest()
+    request.open('GET', url)
+    request.onload = () => {
+      if (request.status === 200) {
+        resolve(request.response)
+      } else {
+        reject(console.log('uh oh'))
+      }
     }
-    releaseDate = releaseDate.filter(function (i) {
+    request.onerror = () => {
+      reject(console.log('uh oh'))
+    }
+    request.send()
+  })
+}
+
+// Get credits & sort them
+
+get('https://api.themoviedb.org/3/person/1108120/movie_credits?api_key=f8e4d97cbdd172b25e1dd31546263dcd&language=en-US')
+  .then((response) => {
+    const credits = JSON.parse(response)
+    let releaseDate = credits.cast.map((i) => {
+      return i.release_date
+    })
+    releaseDate = releaseDate.filter((i) => {
       return !(i === '')
     })
     releaseDate = releaseDate.sort()
-    release = releaseDate.find(nextReleaseDate)
-    releaseIndex = releaseDate.findIndex(nextReleaseDate) + 1
+    return {
+      releaseDate: releaseDate,
+      credits: credits.cast
+    }
+  })
+  // Compare releaseDate array to current date
+  .then((Object) => {
+    const date = getDate()
+    let release = Object.releaseDate.find((element) => {
+      return element > date
+    })
+    const releaseIndex = Object.releaseDate.findIndex((element) => {
+      return element > date
+    }) + 1
     if (date < release) {
-      movieAjaxCall()
+      // Run countdown timer + Run further information calls
+      countdown(release)
+      setInterval(() => {
+        countdown(release)
+      }, 1000)
+      return Object.credits[releaseIndex].id
     } else {
+      // Tell the user there are no upcoming movies
       document.getElementById('stylesheet').href = '/style.css'
       document.querySelector('section').innerHTML = '<div class="container"> <div class="flex-left"> <h1> Alia Bhatt has no movies with a release date announced coming up!</h1></div> <div class="flex-right"></div></div>'
     }
-    countdown()
-    setInterval(countdown, 1000)
-  }
-}
+  })
+  .then((movieID) => {
+    // Get & change movie data
+    get('https://api.themoviedb.org/3/movie/' + movieID + '?api_key=f8e4d97cbdd172b25e1dd31546263dcd&language=en-US')
+      .then((response) => {
+        let movieInfo = JSON.parse(response)
+        document.body.style.backgroundImage = 'url(https://image.tmdb.org/t/p/w533_and_h300_bestv2' + movieInfo.backdrop_path + ')'
+        document.querySelectorAll('[data-title]').forEach(function (i) {
+          i.textContent = movieInfo.title
+        })
+        document.querySelector('[data-description]').textContent = movieInfo.overview
+        document.querySelector('[data-link]').innerHTML = '<a href="http://imdb.com/title/' + movieInfo.imdb_id + '">IMDb</a>'
+      })
+    get('https://api.themoviedb.org/3/movie/' + movieID + '/credits?api_key=f8e4d97cbdd172b25e1dd31546263dcd&language=en-US')
+      .then((response) => {
+        let creditsInfo = JSON.parse(response)
+        document.querySelector('[data-director]').textContent = creditsInfo.crew[0].name
 
-function getDate() {
+        let cast = creditsInfo.cast
+        let aliaIndex = cast.findIndex(function (element) {
+          return element.name === 'Alia Bhatt'
+        })
+        cast.splice(aliaIndex, 1)
+        document.querySelector('[data-stars').textContent = cast[0].name + ', ' + cast[1].name + ', and ' + cast[2].name
+      })
+    get('https://api.themoviedb.org/3/movie/' + movieID + '/videos?api_key=f8e4d97cbdd172b25e1dd31546263dcd&language=en-US')
+      .then((response) => {
+        const videoObject = JSON.parse(response)
+        const videoInfo = videoObject.results
+        if (videoInfo[0] !== undefined) {
+          const trailer = document.createElement('p')
+          const trailerText = document.createTextNode('Watch the trailer on YouTube below:')
+          trailer.appendChild(trailerText)
+          document.querySelector('#youtube').parentElement.insertBefore(trailer, document.querySelector('#youtube'))
+          document.querySelector('#youtube').classList.add('video-container')
+          document.querySelector('#youtube').innerHTML = '<iframe src="https://www.youtube.com/embed/' + videoInfo[0].key + '" frameborder="0" allowfullscreen</iframe>'
+        }
+      })
+  })
+
+  // Rewrite document to tell the user to attempt a refresh
+  .catch(() => {
+    document.getElementById('stylesheet').href = '/style.css'
+    document.querySelector('section').innerHTML = "<div class='container'> <div class='flex-left'> <h1>Uh oh! Your request didn't load. Try a refresh! </h1></div> <div class='flex-right'></div></div>"
+  })
+
+// Used above to compare dates
+
+function getDate () {
   let getDate = new Date()
   let year = getDate.getFullYear()
   let month = 1 + getDate.getMonth()
   let monthDate = getDate.getDate()
   month = ('0' + month).slice(-2)
   monthDate = ('0' + monthDate).slice(-2)
-  date = (year + '-' + month + '-' + monthDate)
-}
-
-// Used above for Array.find
-
-function nextReleaseDate(element) {
-  return element > date
-}
-
-// Get & print movie data after the above functions run
-
-function movieAjaxCall() {
-  const movieID = credits.cast[releaseIndex].id
-  movieRequest = new XMLHttpRequest()
-  movieRequest.open('GET', ('https://api.themoviedb.org/3/movie/' + movieID + '?api_key=f8e4d97cbdd172b25e1dd31546263dcd&language=en-US'))
-  movieRequest.send()
-  movieRequest.onreadystatechange = getMovieInfo
-
-  movieCredits = new XMLHttpRequest()
-  movieCredits.open('GET', ('https://api.themoviedb.org/3/movie/' + movieID + '/credits?api_key=f8e4d97cbdd172b25e1dd31546263dcd&language=en-US'))
-  movieCredits.send()
-  movieCredits.onreadystatechange = getMovieCredits
-
-  movieVideo = new XMLHttpRequest()
-  movieVideo.open('GET', ('https://api.themoviedb.org/3/movie/' + movieID + '/videos?api_key=f8e4d97cbdd172b25e1dd31546263dcd&language=en-US'))
-  movieVideo.send()
-  movieVideo.onreadystatechange = getMovieVideo
-}
-
-function getMovieInfo() {
-  if (movieRequest.readyState === XMLHttpRequest.DONE) {
-    let movieInfo = JSON.parse(movieRequest.response)
-    document.body.style.backgroundImage = 'url(https://image.tmdb.org/t/p/w533_and_h300_bestv2' + movieInfo.backdrop_path + ')'
-    document.querySelectorAll('[data-title]').forEach(function (i) {
-      i.textContent = movieInfo.title
-    })
-    document.querySelector('[data-description]').textContent = movieInfo.overview
-    document.querySelector('[data-link]').innerHTML = '<a href="http://imdb.com/title/' + movieInfo.imdb_id + '">IMDb</a>'
-  }
-}
-
-function getMovieCredits() {
-  if (movieCredits.readyState === XMLHttpRequest.DONE) {
-    let creditsInfo = JSON.parse(movieCredits.response)
-    document.querySelector('[data-director]').textContent = creditsInfo.crew[0].name
-
-    let cast = creditsInfo.cast
-    let aliaIndex = cast.findIndex(function (element) {
-      return element.name === 'Alia Bhatt'
-    })
-    cast.splice(aliaIndex, 1)
-    document.querySelector('[data-stars').textContent = cast[0].name + ', ' + cast[1].name + ', and ' + cast[2].name
-  }
-}
-
-function getMovieVideo() {
-  if (movieVideo.readyState === XMLHttpRequest.DONE) {
-    const videoObject = JSON.parse(movieVideo.response)
-    const videoInfo = videoObject.results
-    if (videoInfo[0] !== undefined) {
-      const trailer = document.createElement('p')
-      const trailerText = document.createTextNode('Watch the trailer on YouTube below:')
-      trailer.appendChild(trailerText)
-      document.querySelector('#youtube').parentElement.insertBefore(trailer, document.querySelector('#youtube'))
-      document.querySelector('#youtube').classList.add('video-container')
-      document.querySelector('#youtube').innerHTML = '<iframe src="https://www.youtube.com/embed/' + videoInfo[0].key + '" frameborder="0" allowfullscreen</iframe>'
-    }
-  }
+  return (year + '-' + month + '-' + monthDate)
 }
 
 // Countdown timer
 
-function countdown() {
+function countdown (release) {
   const t = Date.parse(release) - Date.parse(new Date())
   const days = Math.floor(t / (1000 * 60 * 60 * 24))
   const hours = Math.floor(t / (1000 * 60 * 60) % 24)
   const minutes = Math.floor((t / 1000 / 60) % 60)
   const seconds = Math.floor((t / 1000) % 60)
 
-  function printClock() {
+  function printClock () {
     document.querySelector('[data-days]').textContent = ('0' + days).slice(-2)
     document.querySelector('[data-hours]').textContent = ('0' + hours).slice(-2)
     document.querySelector('[data-minutes]').textContent = ('0' + minutes).slice(-2)
